9de53215d729961ec0ceb43184dbf8720b5cb4c4
Roselma Mendes <rmendes@thoughtworks.com>
2014-12-21 16:33:59 -0300

    Refactoring myFirstIO, and made the my first async IO exercise.

---
+++ myFirstAsyncIO.js
@@ -0,0 +1,9 @@
+var FIRST_ARGUMENT_IN_COMMAND_LINE = 2;
+var MAKING_RIGHT_THE_COUNTING = 1;
+var fs = require('fs');
+var file = process.argv[FIRST_ARGUMENT_IN_COMMAND_LINE];
+fs.readFile(file,"utf8",function(err,data){
+	if(err) throw err;
+	var lines = data != ""?data.split("\n"):{};
+	console.log(lines.length == undefined?"":lines.length - MAKING_RIGHT_THE_COUNTING);
+});
--- myFirstIO.js
+++ myFirstIO.js
@@ -1,4 +1,6 @@
-var argument = process.argv[2];
+var FIRST_ARGUMENT_IN_COMMAND_LINE = 2;
+var MAKING_RIGHT_THE_COUNTING = 1;
+var argument = process.argv[FIRST_ARGUMENT_IN_COMMAND_LINE];
 
 var fs = require('fs');
 
@@ -6,4 +8,4 @@ var fileText = fs.readFileSync(argument,'utf8');
 
 var lines = fileText != ""?fileText.split('\n'):{};
 
-console.log(lines.length == undefined?"":lines.length - 1);
+console.log(lines.length == undefined?"":lines.length - MAKING_RIGHT_THE_COUNTING);
