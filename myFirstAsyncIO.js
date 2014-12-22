a6ec67e5802a715a5006dc4c86035cf662f80799
Roselma Mendes <rmendes@thoughtworks.com>
2014-12-22 16:11:21 -0300

    Changed myFirstAsynIO using const instead var for FIRST_ARGUMENT_IN_COMMAND_LINE.

--- myFirstAsyncIO.js
+++ myFirstAsyncIO.js
@@ -1,5 +1,5 @@
-var FIRST_ARGUMENT_IN_COMMAND_LINE = 2;
-var MAKING_RIGHT_THE_COUNTING = 1;
+const FIRST_ARGUMENT_IN_COMMAND_LINE = 2;
+const MAKING_RIGHT_THE_COUNTING = 1;
 var fs = require('fs');
 var file = process.argv[FIRST_ARGUMENT_IN_COMMAND_LINE];
 fs.readFile(file,"utf8",function(err,data){
